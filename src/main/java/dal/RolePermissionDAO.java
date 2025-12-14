a2725091a35e2fc0384f097776acdc3e2fdf45c5
Phương Đăng <ph09102003@gmail.com>
2025-12-15 00:29:02 +0700

    Update View List role Permission

---
+++ src/main/java/dal/RolePermissionDAO.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import model.RolePermission;
+import model.Roles;
+
+/**
+ *
+ * @author admin
+ */
+public class RolePermissionDAO extends DBContext {
+
+    public List<RolePermission> getRolePermission() {
+        List<RolePermission> rolePermissions = new ArrayList<>();
+        String query = "SELECT rl.id, rl.router, r.id as roleId, r.description, r.name, r.status FROM role_permission as rl JOIN roles as r ON rl.role_id = r.id";
+        try (PreparedStatement ps = connection.prepareStatement(query);
+                ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                RolePermission rp = new RolePermission();
+                rp.setId(rs.getInt("id"));
+                rp.setRouter(rs.getString("router"));
+
+                Roles role = new Roles();
+                role.setId(rs.getInt("roleId"));
+                role.setName(rs.getString("name"));
+                role.setDescription(rs.getString("description"));
+                role.setStatus(rs.getBoolean("status"));
+                rp.setRoles(role);
+
+                    rolePermissions.add(rp);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rolePermissions;
+    }
+
+}
---
+++ src/main/webapp/admin/role/updateRolePermission.jsp
@@ -0,0 +1,16 @@
+<%-- 
+    Document   : updateRolePermission
+    Created on : Dec 15, 2025, 12:17:21 AM
+    Author     : admin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<jsp:include page="../adminLayout.jsp">
+    <jsp:param name="pageTitle" value="Quản lý Permission theo Role" />
+</jsp:include>
+
+<body>
+    <h1>Hello World!</h1>
+</body>
+<jsp:include page="../adminFooter.jsp" />
